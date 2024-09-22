9ecdabf542e34c659c9b1c1c72da46c3ac5a11af
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-09-22 22:04:13 +0200

    Implement main parsing logic

--- InstaParser.CLI/Program.cs
+++ InstaParser.CLI/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using InstaParser.CLI;
+using InstaParser.CLI.Contracts;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+Console.Title = ApplicationDefaults.ApplicationName;
+
+string followersJson = File.ReadAllText(
+    Path.Combine(Directory.GetCurrentDirectory(),
+        ApplicationDefaults.FollowersPath));
+
+string followingJson = File.ReadAllText(
+    Path.Combine(Directory.GetCurrentDirectory(), 
+        ApplicationDefaults.FollowingPath));
+
+List<Relationship> followersData = JsonConvert.DeserializeObject<List<Relationship>>(followersJson)!;
+
+InstagramData followingData = JsonConvert.DeserializeObject<InstagramData>(followingJson)!;
+
+List<string> followers = followersData
+    .Select(follower => follower.Metadata[0].Value)
+    .ToList();
+    
+List<string> following = followingData.Relationships
+    .Select(following => following.Metadata[0].Value)
+    .ToList();
+
+List<string> unfollowed = following.Except(followers).ToList();
+
+unfollowed.ForEach(Console.WriteLine);
